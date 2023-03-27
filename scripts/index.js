02ca650a1861a66a5a8333eededeb84355818b73
Nastasia <113907396+adnstsia@users.noreply.github.com>
2023-03-27 22:20:25 +0300

    добавить 2 класса

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,92 @@
+const config = {
+  inputErrorClass: "form__content_type_error",
+  formInputErrorClassActive: "form__input-error_active",
+  formInputErrorClassInactive: "form__button_inactive",
+  formSelector: ".form__content",
+  formContentForCheck: "form__content",
+  formButtonSelector: ".form__button",
+  form: ".form",
+};
+
+// валидация форм
+class FormValidator {
+  constructor(config, formElement) {
+    this._config = config;
+    this._formElement = formElement;
+    this._inputList = Array.from(
+      this._formElement.querySelectorAll(this._config.formSelector)
+    );
+    this._buttonElement = this._formElement.querySelector(
+      this._config.formButtonSelector
+    );
+  }
+
+  _showInputError(inputElement, errorMessage) {
+    const errorElement = this._formElement.querySelector(
+      `.${inputElement.id}-error`
+    );
+    inputElement.classList.add(this._config.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._config.formInputErrorClassActive);
+  }
+
+  _hideInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(
+      `.${inputElement.id}-error`
+    );
+    inputElement.classList.remove(this._config.inputErrorClass);
+    errorElement.classList.remove(this._config.formInputErrorClassActive);
+    errorElement.textContent = "";
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => !inputElement.validity.valid);
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._buttonElement.classList.add(
+        this._config.formInputErrorClassInactive
+      );
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(
+        this._config.formInputErrorClassInactive
+      );
+      this._buttonElement.disabled = false;
+    }
+  }
+
+  _setEventListeners() {
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+    this._toggleButtonState();
+  }
+
+  enableValidation() {
+    this._formElement.addEventListener("submit", (evt) => evt.preventDefault());
+    this._setEventListeners();
+  }
+}
+
+const forms = Array.from(document.querySelectorAll(config.form));
+forms.forEach((formElement) => {
+  const formValidator = new FormValidator(config, formElement);
+  formValidator.enableValidation();
+});
+
 // Массив карточек
 const initialCards = [
   {
@@ -8,8 +97,10 @@ const initialCards = [
     name: "Уиндермир",
     link: "https://images.unsplash.com/photo-1674468391816-c99157eb212e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=382&q=80",
   },
+
   {
     name: "Мон-Фалер, Италия",
+
     link: "https://images.unsplash.com/photo-1675191475318-d2bf6bad1200?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
   },
   {
@@ -28,8 +119,6 @@ const initialCards = [
 
 const buttonOpenAddCard = document.querySelector(".add-button");
 const cardsContainer = document.querySelector(".cards");
-const cardTemplate = document.querySelector("#card").content;
-const cardForClone = cardTemplate.querySelector(".card");
 const avatarName = document.querySelector(".avatar__title");
 const avatarDescription = document.querySelector(".avatar__subtitle");
 const popup = document.querySelector(".popup");
@@ -91,6 +180,10 @@ function disableSubmitButton(button) {
 const openEditPopup = () => {
   formContentFullName.value = avatarName.textContent;
   formContentDescription.value = avatarDescription.textContent;
+
+  // const formValidator = new FormValidator(config, popupEdit);
+  // formValidator.enableValidation();
+
   openPopup(popupEdit);
 };
 
@@ -178,47 +271,118 @@ overlayAdd.addEventListener("mousedown", closeAddPopup);
 overlayImage.addEventListener("mousedown", closeImagePopup);
 
 // создание карточки
-const createCard = (cardData) => {
-  // карточка
-  const cardElement = cardForClone.cloneNode(true);
+// const createCard = (cardData) => {
+//   // карточка
+//   const cardElement = cardForClone.cloneNode(true);
 
-  const cardImage = cardElement.querySelector(".card__image");
-  cardImage.src = cardData.link;
-  cardImage.alt = cardData.name;
-  cardElement.querySelector(".card__title").textContent = cardData.name;
+//   const cardImage = cardElement.querySelector(".card__image");
+//   const cardTitle = cardElement.querySelector(".card__title");
+//   cardImage.src = cardData.link;
+//   cardImage.alt = cardData.name;
+//   cardTitle.textContent = cardData.name;
+
+//   // сердечко
+//   cardElement
+//     .querySelector(".card__heart")
+//     .addEventListener("click", function (evt) {
+//       evt.target.classList.toggle("card__heart_active");
+//     });
+
+//   // удаление карточки
+//   cardElement.querySelector(".card__trash").addEventListener("click", () => {
+//     cardElement.remove();
+//   });
+
+//   // открытие попапа с фотокарточкой
+//   cardImage.addEventListener("click", function () {
+//     imagePopupPhoto.src = cardImage.src;
+//     imagePopupText.textContent = cardTitle.textContent;
+//     imagePopupPhoto.alt = cardTitle.textContent;
+//     openPopup(popupImage);
+//   });
+
+//   return cardElement;
+// };
+
+class Card {
+  constructor(cardData, cardTemplate) {
+    this._src = cardData.link;
+    this._alt = cardData.name;
+    this._textContent = cardData.name;
+
+    this._cardTemplate = cardTemplate;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardTemplate)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  _setEventListeners = () => {
+    this._cardHeart();
+    this._cardRemove();
+    this._cardOpen();
+  };
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._setEventListeners(); // вызовите _setEventListeners
+
+    this._element.querySelector(".card__image").src = this._src;
+    imagePopupPhoto.src = this._element.querySelector(".card__image").src;
+    this._element.querySelector(".card__image").alt = this._alt;
+    this._element.querySelector(".card__title").textContent = this._textContent;
+    imagePopupText.textContent =
+      this._element.querySelector(".card__title").textContent;
+    imagePopupPhoto.alt =
+      this._element.querySelector(".card__title").textContent;
+    return this._element;
+  }
 
   // сердечко
-  cardElement
-    .querySelector(".card__heart")
-    .addEventListener("click", function (evt) {
-      evt.target.classList.toggle("card__heart_active");
-    });
+  _cardHeart = () => {
+    this._element
+      .querySelector(".card__heart")
+      .addEventListener("click", function (evt) {
+        evt.target.classList.toggle("card__heart_active");
+      });
+  };
 
   // удаление карточки
-  cardElement.querySelector(".card__trash").addEventListener("click", () => {
-    cardElement.remove();
-  });
+  _cardRemove = () => {
+    this._element
+      .querySelector(".card__trash")
+      .addEventListener("click", () => {
+        this._element.remove();
+      });
+  };
 
   // открытие попапа с фотокарточкой
-  cardImage.addEventListener("click", function () {
-    imagePopupPhoto.src = cardElement.querySelector(".card__image").src;
-    imagePopupText.textContent =
-      cardElement.querySelector(".card__title").textContent;
-    imagePopupPhoto.alt = cardElement.querySelector(".card__title").textContent;
-    openPopup(popupImage);
-  });
-
-  return cardElement;
-};
+  _cardOpen = () => {
+    this._element
+      .querySelector(".card__image")
+      .addEventListener("click", function () {
+        openPopup(popupImage);
+      });
+  };
+}
 
 // добавление одной карточки на страницу
-const renderCard = (cardData) => {
-  cardsContainer.prepend(createCard(cardData));
+const renderCard = (item) => {
+  const card = new Card(item, "#card");
+  cardsContainer.prepend(card.generateCard());
 };
 
 // добавление имеющихся в изначальном массиве карточек на страницу
 cardsContainer.append(
-  ...initialCards.map((cardData) => {
-    return createCard(cardData);
+  ...initialCards.map((item) => {
+    const card = new Card(item, "#card");
+    const cardElement = card.generateCard();
+
+    return cardElement;
   })
 );
