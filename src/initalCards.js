ff272b93ff9b6405c807f5e6a22957c93bff0554
Nastasia <113907396+adnstsia@users.noreply.github.com>
2024-04-04 13:17:25 +0300

    design

--- src/initalCards.js
+++ src/initalCards.js
@@ -1,30 +1,40 @@
 // Массив исходных карточек
 const initialCards = [
   {
-    name: "Кинкаку-дзи",
-    link: "https://images.unsplash.com/photo-1675384201055-a5d68374fb7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    name: "Clouds",
+    link: "https://i.pinimg.com/474x/45/a5/3e/45a53ec76708d9fd6152b07b5d067132.jpg",
   },
   {
-    name: "Уиндермир",
-    link: "https://images.unsplash.com/photo-1674468391816-c99157eb212e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=382&q=80",
-  },
-
-  {
-    name: "Мон-Фалер, Италия",
-
-    link: "https://images.unsplash.com/photo-1675191475318-d2bf6bad1200?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+    name: "Vietnamese cuisine",
+    link: "https://i.pinimg.com/474x/e0/a9/13/e0a9136ebbda250de37ccd01cfa59b24.jpg",
   },
   {
-    name: "Плотина Гувера, США",
-    link: "https://images.unsplash.com/photo-1674973188795-d692174fff62?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    name: "Skate season",
+    link: "https://i.pinimg.com/736x/a7/fe/ac/a7feacff43479d0a77b834d3aaa24871.jpg",
   },
   {
-    name: "Сан-Франциско, США",
-    link: "https://images.unsplash.com/photo-1661206444414-d440660c9ff0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    name: "Gingerbreads",
+    link: "https://i.pinimg.com/474x/b5/1f/da/b51fda9e056629f88b137e5baaf9f966.jpg",
   },
   {
-    name: "Озеро Морейн, Канада",
-    link: "https://images.unsplash.com/photo-1536637706725-c96e8837df7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    name: "funny socks",
+    link: "https://i.pinimg.com/474x/c3/39/fc/c339fc84cdc64e3eddd6135cc8104e5d.jpg",
+  },
+  {
+    name: "Snowflakes!!",
+    link: "https://i.pinimg.com/736x/56/fe/e5/56fee5755fd05b99e1c0db6712ac458c.jpg",
+  },
+  {
+    name: "Armenia",
+    link: "https://i.pinimg.com/474x/0b/6e/79/0b6e7993a4b600486cb59618b59abd8f.jpg",
+  },
+  {
+    name: "green-yellow-red",
+    link: "https://i.pinimg.com/474x/46/cb/67/46cb67c41b4b5cf7fdfef2e8ec29df32.jpg",
+  },
+  {
+    name: "refill station",
+    link: "https://i.pinimg.com/564x/86/e3/fc/86e3fcd51f5ed17a0af5d17ca5d55f2d.jpg",
   },
 ];
 
